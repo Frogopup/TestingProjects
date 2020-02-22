fe2649ecac4448709238ae6c5f159db0078a3399
Frogopup <61351938+Frogopup@users.noreply.github.com>
2020-02-22 16:08:47 +0300

    Add files via upload

---
+++ UnitTest_Chat/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_Chat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest_Chat")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("74135808-cbf4-49f7-acec-d4f12addeefb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest_Chat/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChatServer;
+using System.Net.Sockets;
+using System.Text;
+using System.Net;
+
+namespace UnitTest_Chat
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Message_ConnectTest()
+        {
+            Socket testSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            ChatServer.ChatServer cs = new ChatServer.ChatServer(7777);
+            cs.StartListening();
+
+            testSocket.Connect("127.0.0.1", 7777);
+            Net_SendData(testSocket, "TestUsername");
+            string resp = Net_RecieveData(testSocket);
+
+
+            //string resp = Net_RecieveData(testSocket);
+            Assert.AreEqual(resp, "1");
+        }
+
+
+        private void Net_SendData(Socket s, string data)
+        {
+            s.Send(Encoding.Unicode.GetBytes(data));
+        }
+
+        private string Net_RecieveData(Socket s)
+        {
+            StringBuilder sb = new StringBuilder();
+            byte[] buffer = new byte[256];
+            int bytesRecieved = 0;
+            do
+            {
+                bytesRecieved = s.Receive(buffer);
+                if (bytesRecieved == 0) return null;
+                sb.Append(Encoding.Unicode.GetString(buffer, 0, bytesRecieved));
+            } while (s.Available > 0);
+
+            return sb.ToString();
+        }
+    }
+}
